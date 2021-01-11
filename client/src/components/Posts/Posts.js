9aad722bd3ae652e8c1aced3ae9496940c4375ab
Sabita Neupane <nsabita.sasa@gmail.com>
2021-01-11 22:23:46 +0545

    Refactor lines and spaces

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,7 +6,40 @@ import Post from './Post';
 import useStyles from './styles';
 
 const Posts = ({ setCurrentId }) => {
-    const posts = useSelector((state) => state.posts);
+    // const posts = useSelector((state) => state.posts);
+    const posts = [
+        {
+            _id: 3456789,
+            selectedFile: '23',
+            title: 'Hello World',
+            blog: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            writer: 'John Deo',
+            createdAt: '2011-08-12T20:17:46.384Z',
+            tags: [
+                '1', '2', '3'
+            ]
+        }, {
+            _id: 3456789,
+            selectedFile: '23',
+            title: 'Hello World',
+            blog: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            writer: 'John Deo',
+            createdAt: '2011-08-12T20:17:46.384Z',
+            tags: [
+                '1', '2', '3'
+            ]
+        }, {
+            _id: 3456789,
+            selectedFile: '23',
+            title: 'Hello World',
+            blog: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            writer: 'John Deo',
+            createdAt: '2011-08-12T20:17:46.384Z',
+            tags: [
+                '1', '2', '3'
+            ]
+        }
+    ]
     const classes = useStyles();
     console.log(posts);
     return (
